9a8ca4362f2010a1c725474c4a4eaf792398609b
ningod <ningod5949@126.com>
2019-11-19 20:08:52 +0800

    配置及创建数据库的表

---
+++ sxds_project/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('tinymce/', include('tinymce.urls')),
+    path('user/', include('user.urls')),    # 用户模块
+    path('cart/', include('cart.urls')),    # 购物车模块
+    path('order/', include('order.urls')),  # 订单模块
+    path('', include('goods.urls')),    # 商品模块
+]
